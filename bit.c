a1df78848d0f23b6b2d53475f9d0821b16e7ce44
robertburrowes <r.burrowes@auckland.ac.nz>
2019-01-04 15:46:42 +1300

    Change 32 to size of unsigned int

--- bit.c
+++ bit.c
@@ -1,22 +1,28 @@
 #include "bit.h"
 
+//Utility bit manipulation functions for arrays of unsigned int
+
+//Set a bit in an array of unsigned int
 void setbit(unsigned int *bm, int i)
 {
-	bm[i/32] |= 1 << ( i % 32 );
+	bm[i/(sizeof(unsigned int) * 8)] |= 1 << ( i % (sizeof(unsigned int) * 8) );
 }
 
+//Clear a bit in an array of unsigned int
 void clearbit(unsigned int *bm, int i)
 {
-	bm[i/32] &= ~(1 << ( i % 32 ));	
+	bm[i/(sizeof(unsigned int) * 8)] &= ~(1 << ( i % (sizeof(unsigned int) * 8) ));	
 }
 
+//Test for a bit being set in an array of unsigned int
 int isbitset(unsigned  int *bm, int i)
 {
-	return bm[i/32] & (1 << ( i % 32 ));
+	return bm[i/(sizeof(unsigned int) * 8)] & (1 << ( i % (sizeof(unsigned int) * 8) ));
 }
 
+//Test a bit is not set in an array of unsigned int
 int isbitclear(unsigned int *bm, int i)
 {
-	return (bm[i/32] & (1 << ( i % 32 ))) == 0;
+	return (bm[i/(sizeof(unsigned int) * 8)] & (1 << ( i % (sizeof(unsigned int) * 8) ))) == 0;
 }
 
